68274e29f19b649511a81a0884f539a711b48a98
Gardenrtin21 <76092018+Gardenrtin21@users.noreply.github.com>
2022-11-27 16:07:19 +0700

    Update mm2.js
    Signed-off-by: Gardenrtin21 <76092018+Gardenrtin21@users.noreply.github.com>

--- mm2.js
+++ mm2.js
@@ -1,13 +1,13 @@
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Freelancer", "Random People", "College Student"],
+        strings: ["Freelancer", "Librarian", "College Student"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing2", {
-        strings: ["Freelancer", "Random People", "College Student"],
+        strings: ["Freelancer", "Librarian", "College Student"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
